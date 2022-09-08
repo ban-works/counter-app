413ef3cf411360c51d0bcc72d9cc37b1ee8026a8
Alban Fresil <alban.fresil@gmail.com>
2022-09-08 11:37:40 +0200

    added CounterBlockProps interface

--- components/CounterBlock.tsx
+++ components/CounterBlock.tsx
@@ -3,17 +3,23 @@ import Counter from '../components/counter'
 import AddButton from '../components/add-button'
 import ResetButton from '../components/reset-button'
 
+interface CounterBlockProps{
+  firstButton?:number;
+  secondButton?:number;
+}
 
-function CounterBlock(props) {
+function CounterBlock(props:CounterBlockProps) {
 
+  // const {firstButton, secondButton}= props;
   const [count, setCount] = useState(0)
 
-  var updateCount= (value: number) =>{
+  var updateCount = (value:number) =>{
     setCount(count+value)
   }
   var resetCount = ()=>{
     setCount(0)
   }
+  // display conditionnal
 
     return (
       <div>    
